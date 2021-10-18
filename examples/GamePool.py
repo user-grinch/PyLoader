1c3f7a73f25e900a2d4a42c9396584a436aa2ffd
Grinch_ <user.grinch@gmail.com>
2021-10-18 14:06:09 +0600

    Fix gamepool.py crash

--- examples/GamePool.py
+++ examples/GamePool.py
@@ -1,14 +1,14 @@
 import libstd.common as common
 import libstd.opcodes as opcodes
 import libstd.pool as pool
-import libstd.cleo as cleo
+import libstd.addition as add
 
 while True:
-    if cleo.test_cheat("peds"):
+    if pool.test_cheat("peds"):
         for ped in pool.ped():
             print(ped)   
 
-    if cleo.test_cheat("vehs"):
+    if pool.test_cheat("vehs"):
         for handle in pool.veh():
             coord = opcodes.get_car_coordinates(handle)
             print("Veh handle: {} Coords: {}, {}, {}".format(handle, coord[0],coord[1],coord[2]))
